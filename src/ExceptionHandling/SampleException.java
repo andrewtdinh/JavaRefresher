19dfd29474702a77ddf836b972c97c393e8d6272
Andrew Dinh <andrewdinh74@yahoo.com>
2024-02-12 10:39:53 -0800

    Using try-catch block example

---
+++ src/ExceptionHandling/SampleException.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class SampleException {
+
+	public static void main(String[] args) {
+		try {
+			int value = 100;
+			int result = value/0;
+
+			System.out.println(result);
+		} catch(Exception e) {
+			System.out.println("An error was thrown.");
+		}
+		
+	}
+
+}

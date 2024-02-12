76c6928efd3e9bf1f288e26076eb07f01f997494
Andrew Dinh <andrewdinh74@yahoo.com>
2024-02-12 13:51:06 -0800

    Add example of finally block

---
+++ src/ExceptionHandling/FinallyBlock.java
@@ -0,0 +1,19 @@
+package ExceptionHandling;
+
+public class FinallyBlock {
+
+	public static void main(String[] args) {
+		try {
+			String text = "Andrew";
+			System.out.println(text);
+			
+			String text1 = null;
+			System.out.println(text1.length());
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			System.out.println("Inside Finally block");
+		}
+	}
+
+}

13230587a9d28ad575114fe343ff40048cb0c355
Andrew Dinh <andrewdinh74@yahoo.com>
2024-02-12 08:41:04 -0800

    Add a couple more examples of String class methods

---
+++ src/String/StringMethodPartIII.java
@@ -0,0 +1,25 @@
+package String;
+
+public class StringMethodPartIII {
+
+	public static void main(String[] args) {
+		
+//		String text = "Hello Team";
+		String sample = "His name is John and he is a man.";
+		
+//		String text1 = text.replace("e", "a");
+//		String sample1 = sample.replace("is", "was");
+//		
+//		System.out.println(text1);
+//		System.out.println(sample1);
+		
+//		String[] sampletest = sample.split("is");
+//		
+//		for (int i = 0; i < sampletest.length; i++) {
+//			System.out.println(sampletest[i]);
+//		}
+		
+		System.out.println(sample.startsWith("His"));
+	}
+
+}

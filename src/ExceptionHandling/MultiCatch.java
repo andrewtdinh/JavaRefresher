01e1d3408d7d7855c014178ad6615799ea6b155d
Andrew Dinh <andrewdinh74@yahoo.com>
2024-02-12 13:31:06 -0800

    Add example of nested try-catches

--- src/ExceptionHandling/MultiCatch.java
+++ src/ExceptionHandling/MultiCatch.java
@@ -3,7 +3,15 @@ package ExceptionHandling;
 public class MultiCatch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			int value = 100;
+			int result = value / 0;
+		} catch (ArithmeticException e) {
+			System.out.println("Catch specific exception");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Catch generic exception");
+		}
 
 	}
 
---
+++ src/ExceptionHandling/NestedTryCatch.java
@@ -0,0 +1,36 @@
+package ExceptionHandling;
+
+public class NestedTryCatch {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("First Try Block");
+			
+			String text = null;
+			System.out.println(text.length());
+			
+			try {
+				System.out.println("Second Try Block");
+				int value = 100/0;
+				
+				try {
+					System.out.println("Third Try Block");
+					int[] array = new int[3];
+					array[5] = 100;
+					
+				} catch (Exception e) {
+					System.out.println("End of Third Try Block");
+					e.printStackTrace();
+				}
+			} catch(Exception e) {
+				System.out.println("End of Second Try Block");
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			System.out.println("End of first Try Block");
+			e.printStackTrace();
+			
+		}
+	}
+
+}

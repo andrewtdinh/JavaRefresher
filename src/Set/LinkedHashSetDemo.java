955c6fa81a2304fe48f936ea5853fb49d722aa92
Andrew Dinh <andrewdinh74@yahoo.com>
2024-02-17 11:09:18 -0800

    Add example of LinkedHashSet

---
+++ src/Set/LinkedHashSetDemo.java
@@ -0,0 +1,19 @@
+package Set;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet lhs = new LinkedHashSet();
+		
+		lhs.add("123");
+		lhs.add("456");
+		lhs.add("Andrew");
+		lhs.add("Dinh");
+		
+		System.out.println(lhs);
+
+	}
+
+}
